fc805f6268559fad7f9167ebc7e1e21cb14cc6c5
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-08-07 12:19:57 +0530

    Add files via upload

---
+++ chapter1.js
@@ -0,0 +1,17 @@
+//Create a variable of type string andtry to add a numbers to it
+
+// Creating a variable of type string
+let stringVar = "The answer is ";
+// Creating a variable of type number
+let numberVar = 42;
+// Adding the string and the number
+let combined = stringVar + numberVar;
+console.log(combined);  // Output: The answer is 42
+
+//Write a JavaScript program to create a word meaning dictorary of five words
+
+let obj = {
+    name: "Yokshita",
+    stream: "Computer Science"
+}
+console.log(obj);
