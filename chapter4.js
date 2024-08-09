c72c795b7a324ddfb2049c825db7923ac5d807f0
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-08-09 14:23:25 +0530

    Add files via upload

---
+++ chapter4.js
@@ -0,0 +1,14 @@
+//Q1- What will the following print in JavaScript?
+
+console.log("Shush\"".length);
+
+//Q2- Explore the includes, startWith & endsWith function of a string
+
+let string = "Yokshita";
+console.log(string.startsWith("Y"));
+console.log(string.endsWith("a"));
+
+//Q3- Write a program to converta given string to lowerCase
+
+let str = "Yokshita";
+console.log(str.toLowerCase());
